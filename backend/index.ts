4c24ace9a1adbaeb22af11ef35b60156017d57ce
morbalint <balint@morasz.hu>
2024-01-17 09:42:59 +0100

    add frontend

--- backend/index.ts
+++ backend/index.ts
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send('Hi!')
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`App listening on PORT ${port}`));
